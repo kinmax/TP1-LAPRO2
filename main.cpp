9c7b19622f65479b5002f69edcb1b53493866455
L3oops <leonardolprr1998@hotmail.com>
2016-10-08 20:06:30 -0300

    MAIN APPLE PEN

--- main.cpp
+++ main.cpp
@@ -4,12 +4,19 @@
 #include "listaDeVeiculos.h"
 #include "listaDeParadas.h"
 #include "listaDeEstacoesDeBike.h"
+#include <iostream>
+#include <string>
 #include <sstream>
 #include <fstream>
 using namespace std;
 
 int main (void)
 {
+	//listaDeVeiculos transportePublico;
+	//listaDeParadas todasAsParadas;
+	//listaDeEstacoesDeBike bikePoa;
+	int opcao = -1;
+	string linha, estacao;
 
 	ifstream paradafile, linhafile, linhaparadafile, bikefile;
 	paradafile.open("paradas.csv", ios::in);
@@ -22,8 +29,34 @@ int main (void)
 		cout << "Um ou mais arquivos não puderam ser abertos" << endl;
 		return 0;
 	}
-	
-	
 
-	return 0;
+while (opcao != 0){
+	cout << "Digite uma opção \n" << endl;
+	cout << "1 - Exibir todas as linhas de ônibus da cidade." << endl;
+	cout << "2 - Exibir todas as paradas de uma linha." << endl;
+	cout << "3 - Exibir paradas com maior número de linhas." << endl;
+	cout << "4 - Exibir todas estações de bicicletas." << endl;
+	cout << "5 - Exibir a parada mais próxima de uma certa estação de bicicleta e suas linhas" << endl;
+	cout << "0 - Sair" << endl;
+	cin >> opcao;
+switch (opcao){
+	case 1: //transportePublico.ListaLinhasDeOnibus();
+		break;
+	case 2: cout << "Digite a linha:" << endl;
+		cin >> linha;
+		//transportePublico.ListaParadasDaLinha(linha);
+		break;
+	case 3: //todasAsParadas.ImprimeUsoDasParadas();
+		break;
+	case 4: //bikePoa.ListaEstacoesDeBike();
+		break;
+	case 5: cout << "Digite a estação de bicicleta:" << endl;
+		cin >> estacao;
+		//bikePoa.ListaParadasProximasDaEstacao(estacao);
+		break;
+	case 0: return 0;
+	default: cout << "Opção Inválida. Digite uma nova opção." << endl;
+}
+}
+return 0;
 }
